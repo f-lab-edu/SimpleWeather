8f493e77f2747819b2419ef0859edd896eafcc59
Nongju-3000 <kangjongwoo333@yonsei.ac.kr>
2025-07-31 22:59:41 +0900

    remove: CitySearchScreen.kt에서 불필요한 import 문 제거 (#5)
    CitySearchScreen.kt 파일에서 사용되지 않는 `androidx.compose.foundation.layout.width` 및 `androidx.compose.material3.CenterAlignedTopAppBar` import 문을 삭제했습니다.

--- app/src/main/java/com/ben/simpleweather/features/search/CitySearchScreen.kt
+++ app/src/main/java/com/ben/simpleweather/features/search/CitySearchScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -20,7 +19,6 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
