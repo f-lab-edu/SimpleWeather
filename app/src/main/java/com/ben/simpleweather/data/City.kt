f174d635ee993baf47da5da430bf80c50899107f
Nongju-3000 <kangjongwoo333@yonsei.ac.kr>
2025-08-06 00:01:35 +0900

    feat: City 및 Coord 데이터 클래스에 @Serializable 어노테이션 추가 (#11)
    `City.kt`와 `Coord.kt` 데이터 클래스에 `@Serializable` 어노테이션을 추가하여 Kotlinx Serialization 라이브러리를 통해 직렬화/역직렬화가 가능하도록 수정했습니다. 또한, `City.kt`의 `nameKo` 필드에 `@SerialName("name_ko")` 어노테이션을 추가하여 JSON 데이터의 `name_ko` 필드와 매핑되도록 했습니다.

--- app/src/main/java/com/ben/simpleweather/data/City.kt
+++ app/src/main/java/com/ben/simpleweather/data/City.kt
@@ -1,8 +1,13 @@
 package com.ben.simpleweather.data
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class City(
     val id: Int,
     val name: String,
+    @SerialName("name_ko")
     val nameKo: String,
     val state: String?,
     val country: String,
