db22867f7d06eefe7f81e6abdce242c3fb88dc94
Nongju-3000 <kangjongwoo333@yonsei.ac.kr>
2025-08-08 00:08:30 +0900

    fix: CachedWeather 주석 제거 (#10)
    `CachedWeather.kt` 파일에서 `timestamp` 필드의 주석을 제거했습니다.

--- app/src/main/java/com/ben/simpleweather/data/CachedWeather.kt
+++ app/src/main/java/com/ben/simpleweather/data/CachedWeather.kt
@@ -4,5 +4,5 @@ import com.ben.simpleweather.data.remote.dto.WeatherResponse
 
 data class CachedWeather(
     val weather: WeatherResponse,
-    val timestamp: Long // 저장된 시간 (System.currentTimeMillis())
+    val timestamp: Long
 )
